d22542fbd357303ad0b9eb6d2eb19a7a68d32929
noemichis <noemi_chis@yahoo.com>
2023-04-13 15:18:08 +0000

    fix heroku host name

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django-blog-wk.heroku.com', 'localhost']
+ALLOWED_HOSTS = ['django-blog-wk.herokuapp.com', 'localhost']
 
 # Application definition
 
